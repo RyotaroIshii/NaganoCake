0b3ccafdd0750760c8edb1899f03b5c8b6263f32
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-09-05 12:48:30 +0000

    [Add] ユーザー側コーディング途中

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
     case resource
     when Customer
-      customer_path
+      customers_path
     when Admin
       admin_path
     end
