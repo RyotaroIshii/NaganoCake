125c98b7f1fddb2d4d08146db8240fd9c6fc14ed
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-09-11 21:43:10 +0000

    [Add] カート画面途中　

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,5 +1,36 @@
 class Public::CartItemsController < ApplicationController
   def index
+    @cart_items = CartItem.all
+  end
+
+  def create
+    @cart_item = CartItem.new(cart_item_params)
+    @cart_item.customer_id = current_customer.id
+    if @cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      @cart_item.amount += params[:cart_item][:number].to_i
+      @cart_item.save
+      redirect_to cart_path
+    else
+      @cart_item.save
+      redirect_to cart_path
+    end
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.update(cart_item_params)
+    redirect_to cart_path
+  end
+
+  def destroy
+    @cart_item = CartItem.find(params[:id])
+    @cart_item.destroy
+    redirect_to cart_path
+  end
+
+  def destroy_all
+    current_customer.cart_items.destroy_all
+    redirect_to cart_path
   end
 
   private
--- app/models/cart_item.rb
+++ app/models/cart_item.rb
@@ -1,2 +1,8 @@
 class CartItem < ApplicationRecord
+
+
+  def subtotal
+    item.with_tax_price * amount
+  end
+
 end
