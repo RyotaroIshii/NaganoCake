66fcf1f000e38a588fc304ed8ba70e1ec310445c
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-09-11 19:15:12 +0000

    [Add] public/cart_items作成　

--- config/routes.rb
+++ config/routes.rb
@@ -19,9 +19,9 @@ Rails.application.routes.draw do
   namespace :admin do
     root to: 'homes#top'
 
-    resources :customers
+    resources :customers, only: [:index, :show, :edit, :update]
 
-    resources :items
+    resources :items, only: [:new, :create, :index, :show, :edit, :update]
 
     get "genres" => "genres#index", as: "genres"
     post "genres" => "genres#create", as: "genre_create"
@@ -45,6 +45,11 @@ Rails.application.routes.draw do
     get 'items' => 'items#index', as: 'index_item'
     get 'items/:id' => 'items#show', as: 'show_item'
 
+    get 'cart_items' => 'cart_items#index', as: 'cart'
+    post "cart_items" => "cart_items#create", as: "create_cart_item"
+    patch "cart_items/:id" => "cart_items#update", as: "update_cart_item"
+    delete "cart_items/destroy_all" => "cart_items#destroy_all", as: "destroy_all_cart_items"
+    delete "cart_items/:id" => "cart_items#destroy", as: "destory_cart_item"
 
     resources :addresses
 
