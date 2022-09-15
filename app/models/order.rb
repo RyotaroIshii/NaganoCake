92157c0fe929cd47a14a315916ce8907a9d9dfac
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-09-15 10:15:32 +0000

    [Add] enum設定　

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+
+
+  enum payment_method: { credit_card: 0, transfer: 1 }
+  enum status: { waiting_for_payment: 0, confirmation: 1, producing: 2, preparation: 3, shipped: 4 }
+
 end
