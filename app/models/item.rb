1e2ec4bfc7eb24fff03d8f506f57df76e78e6201
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-09-18 21:53:02 +0000

    [Add] 修正　

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,7 +5,9 @@ class Item < ApplicationRecord
   belongs_to :genre
 
 
-
+  def get_image(width,height)
+    image.variant(resize_to_limit:[width,height]).processed
+  end
 
   def with_tax_price
     (price * 1.1).floor
