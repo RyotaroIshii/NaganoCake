b008a9eff202677682f2618eede0bffa750ccadd
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-09-20 10:43:28 +0000

    　　[Add] 注文履歴詳細画面作成

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,9 @@
 class Order < ApplicationRecord
-
-
+  
+  belongs_to :customer
+  has_many :items
+  has_many :order_details, dependent: :destroy
+ 
   enum payment_method: { credit_card: 0, transfer: 1 }
   enum status: { waiting_for_payment: 0, confirmation: 1, producing: 2, preparation: 3, shipped: 4 }
 
