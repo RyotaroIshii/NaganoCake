62986b2247fd99c66d151aa1cc274033537c2ef0
RyotaroIshii <108617872+RyotaroIshii@users.noreply.github.com>
2022-09-21 10:47:49 +0000

    [Add]　コントローラー設定

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,26 @@
 class Admin::OrdersController < ApplicationController
+
+
   def show
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
   end
+
+  def update
+    @order = Order.find(params[:id])
+    @order.update(order_params)
+    @order_details = @order.order_details
+  end
+
+  private
+
+  def order_params
+  params.require(:order).permit(:customer_id, :post_code, :address, :name, :shopping_cost, :payment_method, :total_payment, :status)
+  end
+  
+  def order_detail_params
+    params.require(:order_detail).permit(:item_id, :order_id, :amount, :price, :maiking_status)
+  end
+
+
 end
--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,20 +1,65 @@
 class Public::OrdersController < ApplicationController
+
   def new
+    @order = Order.new
   end
 
   def create
+    @order = Order.new(order_params)
+    @order.customer_id = current_customer.id
+    @order.save
+
+    @cart_items.each do |cart_item|
+      @order_detail = OrderDetail.new
+      @order_detail.order_id = @order.id
+      @order_detail.item_id = cart_item.item_id
+      @order_detail.amount = cart_item.amount
+      @order_detail.price = @order.total_payment
+      @order_detail.save
+    end
+
+
   end
 
   def confirm
+    @order = Order.new(order_params)
+    @cart_items = CartItem.all
+    @order.shopping_cost = 800
+    @order.total_payment = @total.to_i + @order.shopping_cost
+    @address = Address.find(params[:order][:address_id])
+
+    if params[:order][:select_address] == "0"
+        @order.post_code = current_customer.post_code
+        @order.address = current_customer.address
+        @order.name = current_customer.last_name + current_customer.first_name
+
+    elsif params[:order][:select_address] == "1"
+        @order.post_code = @address.post_code
+        @order.address = @address.address
+        @order.name = @address.name
+
+    elsif params[:order][:select_address] == "2"
+        @order.post_code = params[:order][:post_code]
+        @order.address = params[:order][:address]
+        @order.name = params[:order][:name]
+    end
   end
 
   def thanks
   end
 
   def index
+    @orders = current_customer.orders.order("created_at DESC")
   end
 
   def show
+     @order = Order.find(params[:id])
   end
-  
+
+  private
+
+  def order_params
+    params.require(:order).permit(:name, :post_code, :address, :shipping_cost, :total_payment, :payment_method, :status)
+  end
+
 end
